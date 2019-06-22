43d9db193ae0ccc487e1d97cfcdbe99de5bf36e2
Domagoj Vuković <off.vukovic@gmail.com>
2019-06-21 20:29:59 -0600

    added repeateing and non repeating todo counting per day category

--- functions/helpers.js
+++ functions/helpers.js
@@ -5,29 +5,106 @@ module.exports.getDataFromSnap = async todoRef => {
     const workRef = todoRef.child("work");
     const miscRef = todoRef.child("misc");
 
-    const morningData = await morningRef.once("value");
-    const dayData = await dayRef.once("value");
-    const eveningData = await eveningRef.once("value");
-    const workData = await workRef.once("value");
-    const miscData = await miscRef.once("value");
+    let morningRepeating = 0;
+    let morningNonRepeating = 0;
+    await morningRef.once("value", snap => {
+        snap.forEach(child => {
+            if (child.val().isRepeating) {
+                morningRepeating++;
+            } else {
+                morningNonRepeating++;
+            }
+        });
+    });
 
-    let morningNum = morningData.numChildren();
-    let dayNum = dayData.numChildren();
-    let eveningNum = eveningData.numChildren();
-    let workNum = workData.numChildren();
-    let miscNum = miscData.numChildren();
+    let dayRepeating = 0;
+    let dayNonRepeating = 0;
+    await dayRef.once("value", snap => {
+        snap.forEach(child => {
+            snap.forEach(child => {
+                if (child.val().isRepeating) {
+                    dayRepeating++;
+                } else {
+                    dayNonRepeating++;
+                }
+            });
+        });
+    });
 
-    let total = morningNum + dayNum + eveningNum + workNum + miscNum;
+    let eveningRepeating = 0;
+    let eveningNonRepeating = 0;
+    await eveningRef.once("value", snap => {
+        snap.forEach(child => {
+            if (child.val().isRepeating) {
+                eveningRepeating++;
+            } else {
+                eveningNonRepeating++;
+            }
+        });
+    });
+
+    let workRepeating = 0;
+    let workNonRepeating = 0;
+    await workRef.once("value", snap => {
+        snap.forEach(child => {
+            if (child.val().isRepeating) {
+                workRepeating++;
+            } else {
+                workNonRepeating++;
+            }
+        });
+    });
+
+    let miscRepeating = 0;
+    let miscNonRepeating = 0;
+    await miscRef.once("value", snap => {
+        snap.forEach(child => {
+            if (child.val().isRepeating) {
+                miscRepeating++;
+            } else {
+                miscNonRepeating++;
+            }
+        });
+    });
 
     let dataObject = {
         count: {
-            total: total,
             categories: {
-                morning: morningNum,
-                day: dayNum,
-                evening: eveningNum,
-                work: workNum,
-                misc: miscNum
+                morning: {
+                    total: morningRepeating + morningNonRepeating,
+                    types: {
+                        repeating: morningRepeating,
+                        nonRepeating: morningNonRepeating
+                    }
+                },
+                day: {
+                    total: dayRepeating + dayNonRepeating,
+                    types: {
+                        repeating: dayRepeating,
+                        nonRepeating: dayNonRepeating
+                    }
+                },
+                evening: {
+                    total: eveningRepeating + eveningNonRepeating,
+                    types: {
+                        repeating: eveningRepeating,
+                        nonRepeating: eveningNonRepeating
+                    }
+                },
+                work: {
+                    total: workRepeating + workNonRepeating,
+                    types: {
+                        repeating: workRepeating,
+                        nonRepeating: workNonRepeating
+                    }
+                },
+                misc: {
+                    total: miscRepeating + miscNonRepeating,
+                    types: {
+                        repeating: miscRepeating,
+                        nonRepeating: miscNonRepeating
+                    }
+                }
             }
         }
     };
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
-const helpers = require("./helpers");
-
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
 admin.initializeApp();
+
+const helpers = require("./helpers");
 
 // Update todo count every time theres an add, delete, update
 exports.countTodos = functions.database
