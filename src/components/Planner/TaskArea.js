3615737f13ba7bdcedc2217e317e1a6ab4275a88
Domagoj Vukovic <off.vukovic@gmail.com>
2019-11-27 00:08:56 +0100

    Restructued TodoCard folder to be more abstract
    Switched TodoCards to Material UI

--- src/components/Planner/TaskArea.js
+++ src/components/Planner/TaskArea.js
@@ -1,24 +1,17 @@
 // Object Imports
 import React from "react";
-import firebase from "../../firebase/Auth";
 import moment from "moment";
 
 // Destructured Imports
-import { Grid } from "semantic-ui-react";
+import { Grid, Typography } from "@material-ui/core";
 import { connect } from "react-redux";
 
 // Component Imports
-import TodoCard from "./TodoCard/TodoCard";
+import TodoCards from "./TodoCards/TodoCards";
 import RemindersList from "./Reminders/RemindersList";
-import AddTodoCardButton from "./TodoCard/Buttons/AddTodoCardButton";
 
 class TaskArea extends React.Component {
     state = {
-        todoCardRef: firebase.database().ref("todo-cards"),
-        currentUser: firebase.auth().currentUser,
-        isInPast: false,
-        todoCards: [],
-
         // Redux Props
         currentDay: this.props.currentDay
     };
@@ -29,76 +22,22 @@ class TaskArea extends React.Component {
         };
     }
 
-    componentDidMount() {
-        this.addListeners();
-        this.fetchTodoCards(this.state);
-    }
-
-    addListeners = () => {
-        this.addTodoCardListener(this.state);
-        this.addRemoveTodoCardListener(this.state);
-    };
-
-    // Listen for todo card additions
-    addTodoCardListener = ({ currentUser, todoCardRef }) => {
-        todoCardRef.child(currentUser.uid).on("child_added", () => {
-            this.fetchTodoCards(this.state);
-        });
-    };
-
-    // Listen for new todo card deletions
-    addRemoveTodoCardListener = ({ currentUser, todoCardRef }) => {
-        todoCardRef.child(currentUser.uid).on("child_removed", () => {
-            this.fetchTodoCards(this.state);
-        });
-    };
-
-    fetchTodoCards = ({ todoCardRef, currentUser }) => {
-        let todoCardHolder = [];
-
-        todoCardRef.child(currentUser.uid).once("value", todoCards => {
-            todoCards.forEach(todoCard => {
-                todoCardHolder.push({
-                    name: todoCard.val().name,
-                    key: todoCard.val().key
-                });
-            });
-            this.setState({ todoCards: todoCardHolder });
-        });
-    };
-
-    renderTodoCards = ({ todoCards }) =>
-        todoCards.map(todoCard => (
-            <TodoCard todoCard={todoCard} key={todoCard.key} />
-        ));
-
     render() {
         const { currentDay } = this.state;
 
         return (
-            <Grid className="planner-area view-height">
-                <Grid.Row className="task-area-header">
-                    <span className="date-with-name">
+            <Grid container>
+                <Grid xs={12} item>
+                    <Typography variant="h4">
                         {moment(currentDay).format("DD/MM/YYYY - dddd")}
-                    </span>
-                </Grid.Row>
-                <Grid.Row className="view-height task-area">
-                    <Grid.Column
-                        className="todo-area view-height"
-                        width={12}
-                        stretched
-                    >
-                        <Grid>
-                            <Grid.Row columns={3}>
-                                {this.renderTodoCards(this.state)}
-                                <AddTodoCardButton />
-                            </Grid.Row>
-                        </Grid>
-                    </Grid.Column>
-                    <Grid.Column width={4} className="reminder-section">
-                        <RemindersList />
-                    </Grid.Column>
-                </Grid.Row>
+                    </Typography>
+                </Grid>
+                <Grid xs={10} item>
+                    <TodoCards />
+                </Grid>
+                <Grid xs={2} item>
+                    <RemindersList />
+                </Grid>
             </Grid>
         );
     }
---
+++ src/components/Planner/TodoCards/TodoCards.js
@@ -0,0 +1,83 @@
+// Object Imports
+import React from "react";
+import firebase from "../../../firebase/Auth";
+
+// Destructured Imports
+import { Grid } from "@material-ui/core";
+
+// Component Imports
+import TodoCardItem from "./TodoCardItem";
+import AddTodoCardButton from "./Buttons/AddTodoCardButton";
+
+class TodoCards extends React.Component {
+    state = {
+        todoCardRef: firebase.database().ref("todo-cards"),
+        currentUser: firebase.auth().currentUser,
+        isInPast: false,
+        todoCards: []
+    };
+
+    componentDidMount() {
+        this.addListeners();
+        this.fetchTodoCards(this.state);
+    }
+
+    addListeners = () => {
+        this.addTodoCardListener(this.state);
+        this.addRemoveTodoCardListener(this.state);
+    };
+
+    // Listen for todo card additions
+    addTodoCardListener = ({ currentUser, todoCardRef }) => {
+        todoCardRef.child(currentUser.uid).on("child_added", () => {
+            this.fetchTodoCards(this.state);
+        });
+    };
+
+    // Listen for new todo card deletions
+    addRemoveTodoCardListener = ({ currentUser, todoCardRef }) => {
+        todoCardRef.child(currentUser.uid).on("child_removed", () => {
+            this.fetchTodoCards(this.state);
+        });
+    };
+
+    // Fetch todo cards from firebase
+    fetchTodoCards = ({ todoCardRef, currentUser }) => {
+        let todoCardHolder = [];
+
+        todoCardRef.child(currentUser.uid).once("value", todoCards => {
+            todoCards.forEach(todoCard => {
+                todoCardHolder.push({
+                    name: todoCard.val().name,
+                    key: todoCard.val().key
+                });
+            });
+            this.setState({ todoCards: todoCardHolder });
+        });
+    };
+
+    // Render todo cards on the screen
+    renderTodoCards = ({ todoCards }) =>
+        todoCards.map(todoCard => (
+            <Grid item xs={4} key={todoCard.key}>
+                <TodoCardItem todoCard={todoCard} />
+            </Grid>
+        ));
+
+    render() {
+        return (
+            <Grid
+                container
+                direction="row"
+                justify="flex-start"
+                alignItems="flex-start"
+                spacing={2}
+            >
+                {this.renderTodoCards(this.state)}
+                <AddTodoCardButton />
+            </Grid>
+        );
+    }
+}
+
+export default TodoCards;
