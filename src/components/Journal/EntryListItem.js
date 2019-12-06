c409f24907c9a8b57ee630d7b037bcb91a92e6b6
Domagoj Vukovic <off.vukovic@gmail.com>
2019-12-06 23:28:18 +0100

    - Switched EntryListItem.js to Material UI

--- src/components/Journal/EntryListItem.js
+++ src/components/Journal/EntryListItem.js
@@ -3,8 +3,18 @@ import React from "react";
 import firebase from "../../firebase/Auth";
 
 // Destructured Imports
-import { List, Icon, Grid } from "semantic-ui-react";
+import {
+    Typography,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton,
+    List
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
+
+// Icon Imports
+import DeleteIcon from "@material-ui/icons/Delete";
 
 // Helper Imports
 import { formatMoment } from "../../helpers/Global";
@@ -27,40 +37,33 @@ class EntryListItem extends React.Component {
     render() {
         const { journalEntry } = this.state;
 
+        let formatedDate = (
+            <Typography>
+                Created at: {formatMoment(journalEntry.createdAt, "DD/MM/YYYY")}
+            </Typography>
+        );
+
         return (
-            <List.Item
-                as={Link}
-                to={`/journal/${journalEntry.key}`}
-                key={journalEntry.key}
-                className="journal-list-item"
-            >
-                <List.Content>
-                    <List.Header>{journalEntry.title}</List.Header>
-                    <List.Description className="journal-list-item-date">
-                        <Grid>
-                            <Grid.Row>
-                                <Grid.Column width={12} float="left">
-                                    <p>
-                                        Created at:{" "}
-                                        {formatMoment(
-                                            journalEntry.createdAt,
-                                            "DD/MM/YYYY"
-                                        )}
-                                    </p>
-                                </Grid.Column>
-                                <Grid.Column width={4} float="right">
-                                    <Icon
-                                        name="remove"
-                                        onClick={() =>
-                                            this.deleteEntry(this.state)
-                                        }
-                                    />
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                    </List.Description>
-                </List.Content>
-            </List.Item>
+            <List>
+                <Link
+                    to={`/journal/${journalEntry.key}`}
+                    key={journalEntry.key}
+                >
+                    <ListItem>
+                        <ListItemText
+                            primary={journalEntry.title}
+                            secondary={formatedDate}
+                        />
+                        <ListItemSecondaryAction>
+                            <IconButton
+                                onClick={() => this.deleteEntry(this.state)}
+                            >
+                                <DeleteIcon />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                </Link>
+            </List>
         );
     }
 }
