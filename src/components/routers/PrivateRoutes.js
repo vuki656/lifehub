88e7e8824c520ff568f61c738e58d8899cdfedca
Domagoj Vukovic <off.vukovic@gmail.com>
2020-02-13 22:27:46 +0100

    - Removed weight and journal pages

--- src/components/routers/PrivateRoutes.js
+++ src/components/routers/PrivateRoutes.js
@@ -4,16 +4,12 @@ import { Route } from "react-router-dom";
 // Component Imports
 import Dashboard from "../Dashboard/Dashboard";
 import Planner from "../Planner/Planner";
-import Journal from "../Journal/Journal";
-import Weight from "../Weight/Weight";
 import Settings from "../Settings/Settings";
 
 const PrivateRouter = () => (
     <div className="main">
         <Route path="/dashboard" exact component={Dashboard} />
         <Route path="/planner" component={Planner} />
-        <Route path="/journal" component={Journal} />
-        <Route path="/weight" component={Weight} />
         <Route path="/settings" component={Settings} />
     </div>
 );
