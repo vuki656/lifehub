91b04d320d70642688ac4bc7b332aff961525a3c
Domagoj <off.vukovic@gmail.com>
2020-04-02 02:48:50 +0200

    - Implemented typescript to client and server

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<p>Hi</p>, document.getElementById("root"))
+ReactDOM.render(<p>Hi</p>, document.getElementById('root'))
