83aea50cd2407accdd6fba43e47c844a70fb6b44
Domagoj <off.vukovic@gmail.com>
2020-04-03 00:16:40 +0200

    - Added scss - Added spacing scss variables

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import './assets/scss/styles.scss'
 import { RegisterPage } from './pages/register'
 
 ReactDOM.render(<RegisterPage />, document.getElementById('root'))
