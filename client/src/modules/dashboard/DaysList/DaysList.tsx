3119ce0c2572d5eeec722e1017defaa871e3eefa
Domagoj <off.vukovic@gmail.com>
2020-04-19 03:08:19 +0200

    - Implementing days list

--- client/src/modules/dashboard/DaysList/DaysList.tsx
+++ client/src/modules/dashboard/DaysList/DaysList.tsx
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import { DaysListItem } from './DaysListItem'
 
@@ -7,18 +8,34 @@ export const DaysList: React.FunctionComponent<{}> = () => {
 
     // Render 30 days from today
     const renderDays = React.useCallback(() => (
-        [...new Array(30)].map((value, index) => {
+        [...new Array(20)].map((value, index) => {
 
             const nextDay = moment().add(index, 'days')
-            return <DaysListItem day={nextDay} key={nextDay.format('x')} />
+            return <DaysListItem day={nextDay} key={nextDay.format('DoddddMMYYYY')} />
         })
     ), [])
 
     return (
         <div className="days-list">
-            <p>OVERDUE</p>
-            <p>test</p>
-            <p>UPCOMING</p>
+            <NavLink
+                to="/dashboard/overdue"
+                name="overdue"
+                title="overdue"
+                className="days-list__item"
+                activeClassName="days-list__item--active"
+            >
+                <p>OVERDUE</p>
+            </NavLink>
+            {renderDays()}
+            <NavLink
+                to="/dashboard/upcoming"
+                name="upcoming"
+                title="upcoming"
+                className="days-list__item"
+                activeClassName="days-list__item--active"
+            >
+                <p>UPCOMING</p>
+            </NavLink>
         </div>
     )
 }
--- client/src/modules/dashboard/DaysList/DaysListItem/DaysListItem.tsx
+++ client/src/modules/dashboard/DaysList/DaysListItem/DaysListItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import { DaysListItemProps } from './DaysListItem.types'
 
@@ -6,6 +7,14 @@ export const DaysListItem: React.FunctionComponent<DaysListItemProps> = (props) 
     const { day } = props
 
     return (
-        <p className="days-list__item">{day.format('DD dd')}</p>
+        <NavLink
+            to={`/dashboard/${day.format('DoddddMMYYYY')}`}
+            name={`/dashboard/${day.format('DoddddMMYYYY')}`}
+            title={day.format('Do dddd MM YYYY')}
+            className="days-list__item"
+            activeClassName="days-list__item--active"
+        >
+            <p>{day.format('DD dd')}</p>
+        </NavLink>
     )
 }
