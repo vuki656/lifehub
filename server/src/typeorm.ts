606b3f9cbf25a94997f67432b656d3d7698016d1
vuki656 <off.vukovic@gmail.com>
2020-06-07 16:07:11 +0200

    fix(backend): fixed db credentials setup

--- server/src/typeorm.ts
+++ server/src/typeorm.ts
@@ -1,32 +1,19 @@
-import config from 'config'
 import {
     ConnectionOptions,
     createConnection,
 } from 'typeorm'
 
 export const createTypeORMConnection = () => {
-    const {
-        type,
-        host,
-        port,
-        username,
-        password,
-        database,
-        synchronize,
-        logging,
-        ssl,
-    } = config.get('databaseConnection')
-
     return createConnection({
-        database,
-        host,
-        logging,
-        password,
-        port,
-        ssl,
-        synchronize,
-        type,
-        username,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        logging: process.env.DB_LOGGING,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        ssl: process.env.DB_SSL,
+        synchronize: process.env.DB_SYNCHRONIZE,
+        type: process.env.DB_TYPE,
+        username: process.env.DB_USERNAME,
 
         // eslint-disable-next-line sort-keys
         entities: [__dirname + '/entities/**/*.entity.ts'],
