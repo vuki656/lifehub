902e73bb4b65ac41fbd6799e3e1483564ae0b52b
vuki656 <off.vukovic@gmail.com>
2020-06-24 22:44:50 +0200

    feature(client/req-headers): send whole user object (username, email, id) with req headers to api

--- client/src/util/apollo.ts
+++ client/src/util/apollo.ts
@@ -1,8 +1,18 @@
 import ApolloClient from 'apollo-boost'
 
+import store from '../redux/store'
+
+const bearerToken = 'Bearer ' + localStorage.getItem('token')
+const fetchedStore = store.getState()
+
 export const client = new ApolloClient({
     request: (operation) => {
-        operation.setContext({ headers: { authorization: localStorage.getItem('token') } })
+        operation.setContext({
+            headers: {
+                token: bearerToken,
+                user: fetchedStore.user.user, // TODO:  refactor to something more sensible
+            },
+        })
     },
     uri: 'http://localhost:4000',
 })
